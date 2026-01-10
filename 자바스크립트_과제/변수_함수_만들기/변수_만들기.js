8df1e5116157011326b5f1c6ce4bc141f00bf32f
sasasak <geun02young@naver.com>
2026-01-10 20:41:55 +0900

    3주차 과제 1. 변수 만들기

---
+++ 자바스크립트_과제/변수_함수_만들기/변수_만들기.js
@@ -0,0 +1,62 @@
+// 1. 영화 제목을 저장할 변수를 선언하고 좋아하는 영화 제목을 할당하세요.
+let favoriteMovieTitle
+favoriteMovieTitle = '라이브 오브 파이'
+console.log(favoriteMovieTitle)
+
+// 2. "빛의 속도(299,792,458 m/s)"를 담을 상수를 선언하세요.
+const SPEED_OF_LIGHT = 299_792_458
+console.log(SPEED_OF_LIGHT + 'm/s')
+
+// 3. "이메일 인증 완료 여부"를 확인하는 불리언 타입 변수를 선언하고 적절한 값을 할당하세요.
+let isEmailVerification = true
+console.log(isEmailVerification)
+
+// 4. "상품 재고 수량(예: 47개)"을 담는 변수를 선언하고 숫자 값을 할당하세요.
+let inventoryCount = 47
+console.log('상품 재고 수량 : ' + inventoryCount + '개')
+
+// 5. "회원 포인트(예: 15,800점)"를 담는 변수를 작성하세요.
+let customerPoint = 15_800
+console.log('회원 포인트 : ' + customerPoint + '점')
+
+// 6. API 서버의 "기본 URL(예: https://api.example.com)을 담는 상수를 작성하고 값을 할당하세요.
+const API_SERVER_URL = "https://api.example.com"
+console.log('기본 URL: ' + API_SERVER_URL)
+
+// 7. 게시글의 "조회수(예: 1,234)"를 담는 변수를 선언하고 값을 할당하세요.
+let views = 777
+console.log('게시글 조회수 : ' + views)
+
+// 8. "배송 상태('준비중', '배송중', '배송완료' 중 하나)"를 담는 변수를 작성해보세요.
+
+const PREPARING_DELIVERY = '배송 준비중'
+const DELIVERY = '배송중'
+const COMPLETE_DELIVERY = '배송 완료'
+
+let deliveryStatus = DELIVERY
+console.log(deliveryStatus)
+
+// 9. "쿠폰 사용 가능 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정하세요.
+let isAvailableCoupon = true
+console.log('쿠폰 사용 가능 여부 : ', isAvailableCoupon)
+isAvailableCoupon = false
+console.log('쿠폰 사용 가능 여부 : ', isAvailableCoupon)
+
+// 10. "최대 업로드 파일 크기(예: 10MB를 바이트 단위로 10485760)"를 담는 상수를 작성하세요.
+const fileSize = '10MB'
+const MAX_FILE_SIZE = parseFloat(FileSize) * (2**20)
+console.log('최대 업로드 파일 크기 : ' + MAX_FILE_SIZE + 'byte')
+
+// 11. "사용자 등급 점수(예: 85.5점)"를 담는 변수를 선언하고 소수점이 포함된 값을 할당하세요.
+let userGradeScore = '92.7점'
+console.log(parseFloat(userGradeScore))
+
+userGradeScore = 88.3
+console.log(userGradeScore)
+
+// 12. "알림 수신 동의 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정하세요.
+let isAgreeNotification = true
+console.log(isAgreeNotification)
+
+isAgreeNotification = false
+console.log(isAgreeNotification)
