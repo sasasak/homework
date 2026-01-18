1b2a72ab7037b6446a7d5b2ba140f047e81c5557
sasasak <geun02young@naver.com>
2026-01-18 21:10:24 +0900

    과제 : 캐러셀 컴포넌트 js 미완성 , 회고 작성

---
+++ 자바스크립트_과제/캐러셀_컴포넌트_만들기/carousel-component.js
@@ -0,0 +1,52 @@
+const gameProfile = [
+  {
+    gameTitle:'크로노오디세이',
+    gameSlogan : '멈춰 있던 시간이 흐른다',
+    gameGenre : 'MMORPG',
+    gamePageLink : 'https://chronoodyssey.kakaogames.com/ko/',
+    backgroundImage: './image/크로노오디세이-배경.webp',
+    characterImage : './image/크로노오디세이-캐릭터.webp'
+  },
+  {
+    gameTitle:'아키에이지 워',
+    gameSlogan : '시대의 전설을 향해',
+    gameGenre : 'MMORPG',
+    gamePageLink : 'https://aw.kakaogames.com/',
+    backgroundImage: './image/아키에이지워-배경.webp',
+    characterImage : './image/아키에이지워-캐릭터.webp'
+  },
+  {
+    gameTitle:'패스 오브 엑자일 2',
+    gameSlogan : '얼리 엑세스 오픈!',
+    gameGenre : '핵앤슬래시',
+    gamePageLink : 'https://pathofexile2.game.daum.net/main',
+    backgroundImage: './image/패스오브액자일2-배경.webp',
+    characterImage : './image/패스오브액자일2-캐릭터.webp'
+  },
+  {
+    gameTitle:'오딘 : 발할라 라이징',
+    gameSlogan : '신의 영역을 차지하라',
+    gameGenre : 'MMORPG',
+    gamePageLink : 'https://odin.game.daum.net/odin/',
+    backgroundImage: './image/오딘발할라라이징-배경.webp',
+    characterImage : './image/오딘발할라라이징-캐릭터.webp'
+    
+  },
+  {
+    gameTitle:'롬 : 리멤버 오브 마제스티',
+    gameSlogan : '세계는 하나의 전장이 된다.',
+    gameGenre : ' MMORPG',
+    gamePageLink : 'https://rom.kakaogames.com/kr/',
+    backgroundImage: './image/리멤버오브마제스타-캐릭터-배경.webp',
+    characterImage : './image/리멤버오브마제스타-캐릭터.webp'
+  },
+  {
+    gameTitle:'배틀그라운드',
+    gameSlogan : '최후까지 생존하라!',
+    gameGenre : '배틀로얄',
+    gamePageLink : 'https://pubg.game.daum.net/pubg/index.daum',
+    backgroundImage: './image/배틀그라운드-배경.webp',
+    characterImage : './image/배틀그라운드-캐릭터.webp'
+  }
+]
+
