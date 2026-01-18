9de5b80761fcfcaa9eca78ab0fab5a74a07db31f
sasasak <geun02young@naver.com>
2026-01-18 21:56:34 +0900

    과제 : 최종 수정 및 회고

--- 자바스크립트_과제/캐러셀_컴포넌트_만들기/carousel-component.js
+++ 자바스크립트_과제/캐러셀_컴포넌트_만들기/carousel-component.js
@@ -1,52 +1,38 @@
-const gameProfile = [
-  {
-    gameTitle:'크로노오디세이',
-    gameSlogan : '멈춰 있던 시간이 흐른다',
-    gameGenre : 'MMORPG',
-    gamePageLink : 'https://chronoodyssey.kakaogames.com/ko/',
-    backgroundImage: './image/크로노오디세이-배경.webp',
-    characterImage : './image/크로노오디세이-캐릭터.webp'
-  },
-  {
-    gameTitle:'아키에이지 워',
-    gameSlogan : '시대의 전설을 향해',
-    gameGenre : 'MMORPG',
-    gamePageLink : 'https://aw.kakaogames.com/',
-    backgroundImage: './image/아키에이지워-배경.webp',
-    characterImage : './image/아키에이지워-캐릭터.webp'
-  },
-  {
-    gameTitle:'패스 오브 엑자일 2',
-    gameSlogan : '얼리 엑세스 오픈!',
-    gameGenre : '핵앤슬래시',
-    gamePageLink : 'https://pathofexile2.game.daum.net/main',
-    backgroundImage: './image/패스오브액자일2-배경.webp',
-    characterImage : './image/패스오브액자일2-캐릭터.webp'
-  },
-  {
-    gameTitle:'오딘 : 발할라 라이징',
-    gameSlogan : '신의 영역을 차지하라',
-    gameGenre : 'MMORPG',
-    gamePageLink : 'https://odin.game.daum.net/odin/',
-    backgroundImage: './image/오딘발할라라이징-배경.webp',
-    characterImage : './image/오딘발할라라이징-캐릭터.webp'
-    
-  },
-  {
-    gameTitle:'롬 : 리멤버 오브 마제스티',
-    gameSlogan : '세계는 하나의 전장이 된다.',
-    gameGenre : ' MMORPG',
-    gamePageLink : 'https://rom.kakaogames.com/kr/',
-    backgroundImage: './image/리멤버오브마제스타-캐릭터-배경.webp',
-    characterImage : './image/리멤버오브마제스타-캐릭터.webp'
-  },
-  {
-    gameTitle:'배틀그라운드',
-    gameSlogan : '최후까지 생존하라!',
-    gameGenre : '배틀로얄',
-    gamePageLink : 'https://pubg.game.daum.net/pubg/index.daum',
-    backgroundImage: './image/배틀그라운드-배경.webp',
-    characterImage : './image/배틀그라운드-캐릭터.webp'
-  }
-]
+const gameLists = document.querySelectorAll('.recommend-game-list')
+const nextButton = document.querySelector('.next-button')
+const backButton = document.querySelector('.back-button')
+const pageButton = document.querySelector('.page-button span')
+let pageNumber = 0
+const MAX_PAGES = 3
 
+const pageConversion = () => {
+  gameLists[0].classList.remove('active')
+  gameLists[1].classList.remove('active')
+  gameLists[2].classList.remove('active')
+
+  if (pageNumber === 0) {
+    gameLists[0].classList.add('active')
+  } else if (pageNumber === 1) {
+    gameLists[1].classList.add('active')
+  } else if (pageNumber === 2) {
+    gameLists[2].classList.add('active')
+  }
+
+  pageButton.textContent = (pageNumber + 1) + "/" + MAX_PAGES
+  
+}
+
+nextButton.addEventListener('click', () => {
+  if (pageNumber < MAX_PAGES - 1) { 
+    pageNumber++; 
+    pageConversion()
+  }
+})
+
+backButton.addEventListener('click', () => {
+  if (pageNumber > 0) { 
+    pageNumber--; 
+    pageConversion()
+  }
+})
+pageConversion()
